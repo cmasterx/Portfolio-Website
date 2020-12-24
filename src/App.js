292dd9e312edc18148438ba2cb3c61f999d5da8a
Charlemagne Wong <frian13@hotmail.com>
2020-12-24 17:27:15 -0600

    Initial push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App() {<br></br>
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="Card">
+        <div className="TitleRow Sentence">
+          <h1 className="Title">Hello</h1>
+          <h1 className="Title yellow">!</h1>
+        </div>
+        <h2 className="bio">
+          I am <span className="yellow">Charlemagne Wong</span>,
+          <br></br>
+          and I am a <span id="category" className="yellow-box">Developer</span>.
+        </h2>
+        <p className="message">
+          I'm still working on this site, so comeback in a little bit!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="icons"></div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+var describer_idx = 0;
+var describer = [
+  'Developer',
+  'Computer Engineer',
+  'Pastry Chef',
+  'Hobbyist',
+  'Student'
+]
+setInterval(() => {
+  let txt = document.getElementById('category')
+  describer_idx = (describer_idx + 1) % describer.length;
+  
+  txt.innerText = describer[describer_idx];
+  
+  console.log("Hello!");
+}, 4000);
