a174a9a08df894a66efcf7d254ae120309fcd9e1
Charlemagne Wong <frian13@hotmail.com>
2020-12-26 00:18:46 -0600

    Update server to use https

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+
+const credentials = {
+  key: fs.readFileSync('sslcert/key.pem', 'utf8'),
+  cert: fs.readFileSync('sslcert/cert.pem', 'utf8')
+};
 
 const app = express();
 
@@ -13,5 +21,9 @@ app.get('/ping', function(req, res) {
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
-  
-app.listen(process.env.PORT || 8080);
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(8080);
+httpsServer.listen(8443);
